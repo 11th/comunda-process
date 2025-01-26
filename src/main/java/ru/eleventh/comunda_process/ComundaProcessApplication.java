e8f853d7f4321100b1dd539540f98c35c7d49c72
11th <11th@bk.ru>
2025-01-26 18:44:09 +0300

    initial commit

---
+++ src/main/java/ru/eleventh/comunda_process/ComundaProcessApplication.java
@@ -0,0 +1,13 @@
+package ru.eleventh.comunda_process;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComundaProcessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComundaProcessApplication.class, args);
+	}
+
+}
